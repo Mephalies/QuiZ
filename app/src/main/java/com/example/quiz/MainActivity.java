ca665129748c27bb4452341750f2b72aeb488e91
Bárbara <babagil@sapo.pt>
2022-01-16 21:37:15 +0000

    Added Layout and interactions, app working

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,59 +2,213 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     int currentQuestionIndex, totalCorrect, totalQuestions;
     ArrayList<Question> questions;
+
+    ImageView questionImageView;
+    TextView questionTextView;
+    TextView questionRemainingTextView;
+    Button answer1Button;
+    Button answer2Button;
+    Button answer3Button;
+    Button answer4Button;
+    Button submitButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        
+        //logo in action bar if activated
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_logo);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setElevation(0);*/
+        Log.d(TAG,"nenter");
+
+        setContentView(R.layout.activity_main);
+
+        // View member variables
+        questionImageView = findViewById(R.id.iv_main_quiz_image);
+        questionTextView = findViewById(R.id.tv_main_quiz_questions);
+        questionRemainingTextView= findViewById(R.id.tv_main_number_questions_remaining);
+        answer1Button = findViewById(R.id.btn_main_question_1);
+        answer2Button = findViewById(R.id.btn_main_question_2);
+        answer3Button = findViewById(R.id.btn_main_question_3);
+        answer4Button = findViewById(R.id.btn_main_question_4);
+        submitButton = findViewById(R.id.btn_main_submit_button);
+
+        // onClickListener for each answer Button
+        answer1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAnswerSelected(1);
+            }
+        });
+        answer2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAnswerSelected(2);
+            }
+        });
+        answer3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAnswerSelected(3);
+            }
+        });
+        answer4Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAnswerSelected(4);
+            }
+        });
+
+        // onClickListener for the submit answer Button
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               onAnswersSubmission();
+            }
+        });
+
+        startNewGame();
+    }
+
+    // Displays image, question and answers
+        void displayQuestion(Question question){
+        questionTextView.setText(question.questionText);
+        questionImageView.setImageResource(question.imageId);
+        answer1Button.setText(question.answer1);
+        answer2Button.setText(question.answer2);
+        answer3Button.setText(question.answer3);
+        answer4Button.setText(question.answer4);
+        }
+
+    // Sets text of remaining questions
+    void displayQuestionsRemaining(int questionRemaining){
+       Log.d(TAG, String.valueOf(questionRemaining));
+        questionRemainingTextView.setText(String.valueOf(questionRemaining));
+    }
+
+    // Checks if answer is correct
+    @SuppressLint("SetTextI18n")
+    void onAnswerSelected(int answerSelected){
+        Question current =getCurrentQuestion();
+        current.playersAnswer=answerSelected;
+        answer1Button.setText(current.answer1);
+        answer2Button.setText(current.answer2);
+        answer3Button.setText(current.answer3);
+        answer4Button.setText(current.answer4);
+        if(answerSelected==1)
+            answer1Button.setText("✔ " + current.answer1);
+        else if(answerSelected==2)
+            answer2Button.setText("✔ " + current.answer2);
+        else if(answerSelected==3)
+            answer3Button.setText("✔ " + current.answer3);
+        else
+            answer4Button.setText("✔ " + current.answer4);
+
+    }
+
     void startNewGame(){
-        currentQuestionIndex=0;
-        totalCorrect=0;
-        Question question1 = new Question(1, "What's my name?1", "dog", "dogitto", "small doggin", ":3", 3);
-        Question question2 = new Question(1, "What's my name?2", "dog", "dogitto", "small doggin", ":3", 2);
-        Question question3 = new Question(1, "What's my name?3", "dog", "dogitto", "small doggin", ":3", 1);
         questions= new ArrayList<>();
+        currentQuestionIndex=0;
+
+        // Amount of questions in game
+        totalQuestions= 3;
+
+
+        // Creates each question
+        Question question1 = new Question(R.drawable.anime1, "What's the name of the Anime1", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 3);
+        Question question2 = new Question(R.drawable.anime1, "What's the name of the Anime2", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 2);
+        Question question3 = new Question(R.drawable.anime1, "What's the name of the Anime3", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 1);
+        Question question4 = new Question(R.drawable.anime1, "What's the name of the Anime4", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 3);
+        Question question5 = new Question(R.drawable.anime1, "What's the name of the Anime5", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 2);
+        Question question6 = new Question(R.drawable.anime1, "What's the name of the Anime6", "Naruto", "One Piece", "Promissed Neverland", "My hero Academia", 1);
+
+
+        //add all questions to list
         questions.add(question1);
         questions.add(question2);
         questions.add(question3);
-        totalQuestions= questions.size();
+        questions.add(question4);
+        questions.add(question5);
+        questions.add(question6);
+
+        // TODO check size of all answers to change the size of box of all if needed
+        //checkSizeOfAnswers(questions);
+
+
+        //initializing
+        totalCorrect=0;
         Question firstQuestion = chooseNewQuestion();
-        Question secondQuestion = chooseNewQuestion();
-        Question thirdQuestion = chooseNewQuestion();
-        // displayQuestion(firstQuestion);
-        // displayQuestionsRemaining(questions.size());
+
+
+        // gets number of question an updates it on screen
+        displayQuestionsRemaining(totalQuestions);
+
+        // displays Question
+        displayQuestion(firstQuestion);
     }
+
+
+
+    // uses the random class to choose a random question
     public  Question chooseNewQuestion(){
-        currentQuestionIndex=generateRandomNumber(totalQuestions);
+        currentQuestionIndex=generateRandomNumber(questions.size());
         return questions.get(currentQuestionIndex);
     }
+
+    //gets current question
     public Question getCurrentQuestion(){
         return questions.get(currentQuestionIndex);
     }
+
+    //After user submits answer everything gets upadated
     public void onAnswersSubmission(){
         Question answer= getCurrentQuestion();
         if(answer.playersAnswer==answer.correctAnswer)
             totalCorrect++;
+        else if(answer.playersAnswer==-1)
+            return;
         questions.remove(answer);
-        // displayQuestionsRemaining(questions.size());
-        if(currentQuestionIndex==totalQuestions) {
-            System.out.println("game over");
+        totalQuestions--;
+
+        // updates number of questions remaining
+        displayQuestionsRemaining(totalQuestions);
+        if(totalQuestions == 0) {
+            // TODO 5-D: Show a new window
+            Log.d(TAG, "Game Over");
             startNewGame();
-            // TODO: uncomment after implementing displayQuestion()
-            // displayQuestion(getCurrentQuestion());
+
 
         }
-
+        else{
+            // randomly chooses new question
+            chooseNewQuestion();
+            // updates question
+            displayQuestion(getCurrentQuestion());
+        }
 
     }
 
--- app/src/main/java/com/example/quiz/Question.java
+++ app/src/main/java/com/example/quiz/Question.java
@@ -2,18 +2,17 @@ package com.example.quiz;
 
 public class Question {
     int correctAnswer, playersAnswer, imageId;
-    String questionText, answer0, answer1, answer2, answer3;
+    String questionText, answer1, answer2, answer3, answer4;
 
 
-    // Question question1 = new Question(1, "What's my name?", "dog", "dogitto", "small doggin", ":3", 3);
 
-    public Question(int imageId, String questionText, String answer0, String answer1, String answer2, String answer3, int correctAnswer) {
+    public Question(int imageId, String questionText, String answer1, String answer2, String answer3, String answer4, int correctAnswer) {
         this.imageId = imageId;
         this.questionText = questionText;
-        this.answer0 = answer0;
         this.answer1 = answer1;
         this.answer2 = answer2;
         this.answer3 = answer3;
+        this.answer4 = answer4;
         this.correctAnswer = correctAnswer;
         this.playersAnswer=-1;
     }
